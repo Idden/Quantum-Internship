a3a7ff8f782e4c7b61db1f7eee68ee39fa2c4110
Idden <idden.tsai@gmail.com>
2025-07-22 17:35:20 -0500

    Eigen
    Fix ^ to **
    Add eigenvalues and eigenvectors

--- hamiltonianPosition.py
+++ hamiltonianPosition.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
 from scipy.sparse import diags
+from scipy.sparse.linalg import eigsh
 
 style.use("dark_background")
 
@@ -17,8 +18,8 @@ x_list = np.linspace(-x_max, x_max, N)
 dx = x_list[1] - x_list[0]
 
 # constants factored out
-T = -hbar^2 / (2 * mass * dx^2)
-U = 0.5 * mass * omega^2
+kineticConstant = -hbar**2 / (2 * mass * dx**2)
+potentialConstant = 0.5 * mass * omega**2
 
 # finite difference coefficient into kinetic matrix
 main_diag = np.full(N, -2)
@@ -26,9 +27,11 @@ off_diag = np.full(N - 1, 1)
 kineticMatrix = diags([off_diag, main_diag, off_diag], offsets=[-1, 0, 1])
 
 # potential matrix
-potentialMatrix = diags(x_list)
+potentialMatrix = diags([x_list], [0])
 
 # hamiltonian -> H = K + P
-H = T * kineticMatrix + U * potentialMatrix
+H = kineticConstant * kineticMatrix + potentialConstant * potentialMatrix
 
-
+# eigenvalues and vectors
+numStates = 5
+eigenvalues, eigenstates = eigsh(H, k=numStates, which='SM')
