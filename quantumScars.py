30568715bac3268014c06bbe691772310c77042e
Idden <idden.tsai@gmail.com>
2025-08-11 23:19:51 -0500

    fix: Turn code into a function

--- quantumScars.py
+++ quantumScars.py
@@ -2,12 +2,16 @@ import math
 
 binNum = "10010"
 
-startBase = math.pow(2, len(binNum)-1)
-deciNum = 0
-
-for i in range(len(binNum)):
-    deciNum += int(binNum[i]) * startBase
-    startBase /= 2
+def binToDeci(num):
     
-print(deciNum)
-print(int(binNum, 2))
+    startBase = math.pow(2, len(num))
+    deciNum = 0
+
+    for i in range(len(num)):
+        deciNum += int(num) * startBase
+        startBase /= 2
+
+    return deciNum
+
+
+
