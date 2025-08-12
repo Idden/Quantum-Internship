a07468c96cd2bb018e7c7efbe8f634b4c90f820f
Idden <idden.tsai@gmail.com>
2025-08-12 16:48:52 -0500

    feature: Find all bit strings with no consecutive ones

--- quantumScars.py
+++ quantumScars.py
@@ -15,3 +15,32 @@ def binToDeci(num):
 
 
 
+def binNoConsecOnes(N):
+
+    if N == 0:
+        return
+
+    listNoConsecOnes = []
+    consecOnes = None
+
+    for i in range(int(math.pow(2, N))):
+        
+        currNum = str(bin(i))[2:]
+        consecOnes = False
+
+        if len(currNum) == 1:
+            listNoConsecOnes.append(currNum)
+            continue
+
+        for j in range(len(currNum)-1):
+
+            if currNum[j] == currNum[j+1] and currNum[j] == "1":
+                consecOnes = True
+                break
+        
+        if consecOnes:
+            continue
+
+        listNoConsecOnes.append(currNum)
+
+    print(listNoConsecOnes)
