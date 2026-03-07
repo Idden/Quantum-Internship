bd0c0fb3c17bacdbcf074bcba1e69d79ba316e73
Idden <idden.tsai@gmail.com>
2026-03-07 11:27:33 -0600

    rerun tests on fixed disorder hamiltonian

--- Quantum_Batteries_Scars/disorder_tests/quantumScarFunctions.py
+++ Quantum_Batteries_Scars/disorder_tests/quantumScarFunctions.py
@@ -55,7 +55,7 @@ def make_coeff(r):
     return lambda t, args: args["A"] * np.cos(args[f"wd{r}"] * t)
 
 
-def get_scar_ham(N, disorder=False, detuning=0.0, random_seed=False):
+def get_scar_ham(N, disorder=False, detuning=0.0, random_seed=False, ds_detuning=0.0, random_drive_strength=False):
     assert (N % 2 == 0), "N must be a multiple of 2"
 
     if not random_seed:
@@ -207,164 +207,28 @@ def get_scar_ham(N, disorder=False, detuning=0.0, random_seed=False):
     copyBasis = basisList
     diagH1 = []
 
+    if random_drive_strength:
+        driveWeights = np.random.uniform(-ds_detuning, ds_detuning, N)
+        driveWeights = 1.0 + driveWeights
+
     # switches 0s to -1s and keeps 1s the same for the copyBasis
     # appends to diagH1 the dot product between each bit string and the 0 -> -1 Z2 state
+    z2bitString = list(z2_initial(N))
+    z2bitString = 2 * np.array([int(i) for i in z2bitString]) - 1
     for i in range(basisLen):
 
         bitString = list(copyBasis[i])
         bitString = [int(i) for i in bitString]
 
-        z2bitString = list(z2_initial(N))
-        z2bitString = 2 * np.array([int(i) for i in z2bitString]) - 1
-
-        diagH1.append(np.dot(2 * np.array(bitString) - 1, z2bitString))
-
+        if random_drive_strength:
+            diagH1.append(np.dot(driveWeights * (2 * np.array(bitString) - 1), z2bitString))
+        else:
+            diagH1.append(np.dot(2 * np.array(bitString) - 1, z2bitString))
     # rows and columns lists for diagonal positions in H1
-    rowH1 = [i for i in range(basisLen)]
-    columnH1 = [i for i in range(basisLen)]
+    diagLocationH1 = [i for i in range(basisLen)]
 
     # creates sparse matrix with diagonals as diagH1 list
-    H1 = csr_matrix((diagH1, (rowH1, columnH1)), shape=[basisLen, basisLen])
-    H1 = qt.Qobj(H1)
-
-    return H0, H1, eigenvalues, eigenstates, psi0, basisList
-
-def get_random_strength_scar_ham(N, d=0.0):
-    assert (N % 2 == 0), "N must be a multiple of 2"
-
-    basisList = binNoConsecOnesEfficient(N)
-    for basis in basisList:
-        if basis[0] == '1' and basis[-1] == '1':
-            basisList.remove(basis)
-        
-    basisMap = {bitStr: i for i, bitStr in enumerate(basisList)}
-    basisLen = len(basisList)
-    flippedList = []
-    ohms = 1.0
-
-    rowBare = []
-    columnBare = []
-
-    rowFactor = []
-    columnFactor = []
-
-    # flip bit hashmap
-    flipMap = {'0': '1', '1': '0'}
-
-    # sigma z op hashmap
-    sigzMap = {'0': '-1', '1': '1'}
-
-    # list of ints for Hamiltonian
-    numList = []
-
-    # -------------------------------
-    #
-    # create the bare PXP hamiltonian
-    #
-    # -------------------------------
-    for i in range(basisLen):
-
-        # add padding so that search doesnt go out of range
-        paddedBitStr = basisList[i][-1] + basisList[i] + basisList[i][0]
-        copyBit = list(paddedBitStr)
-
-        # apply the sum of r P_r-1 * sigma_x * P_r+1 operator
-        for j in range(1, N+1):
-            
-            if paddedBitStr[j-1] == '0' and paddedBitStr[j+1] == '0':
-                copyBit[j] = flipMap[paddedBitStr[j]]
-                flippedList.append(''.join(copyBit)[1:-1])
-                copyBit = list(paddedBitStr)
-            
-        # adds row and column values for the sparse matrix
-        for k in range(len(flippedList)):
-            rowBare.append(basisMap[flippedList[k]])
-            columnBare.append(i)
-            
-        flippedList.clear()
-
-    # -------------------------------
-    #
-    # create the sigma Z PXP hamiltonian perturbation
-    #
-    # -------------------------------
-    for i in range(basisLen):
-
-        # add padding so that search doesnt go out of range
-        paddedBitStr = basisList[i][-2] + basisList[i][-1] + basisList[i] + basisList[i][0] + basisList[i][1]
-        copyBit = list(paddedBitStr)
-        factor = 1
-
-        # apply the PXP operator
-        for j in range(2, N+2):
-            
-            if (paddedBitStr[j-1] == '0') and (paddedBitStr[j+1] == '0'):
-                copyBit[j] = flipMap[paddedBitStr[j]]
-
-                # apply sigmaZ_r-2 + sigmaZ_r+2
-                r_neg2 = int(sigzMap[paddedBitStr[j-2]])
-                r_pos2 = int(sigzMap[paddedBitStr[j+2]])
-                factor = r_neg2 + r_pos2
-                numList.append(factor)
-
-                flippedList.append(''.join(copyBit)[2:-2])
-                copyBit = list(paddedBitStr)
-            
-        # adds row and column values for the sparse matrix
-        for k in range(len(flippedList)):
-            rowFactor.append(basisMap[flippedList[k]])
-            columnFactor.append(i)
-            
-        flippedList.clear()
-
-    # list of ones for the sparse matrix
-    onesList = np.ones(len(rowBare), dtype=int)
-
-    # create the sparse matrix and turn it into a Qobj
-    sparseBareHamiltonian = csr_matrix((onesList, (rowBare, columnBare)), shape=[basisLen, basisLen])
-    sparseFactoredHamiltonian = csr_matrix((numList, (rowFactor, columnFactor)), shape=[basisLen, basisLen])
-    H0 = (ohms / 2 * sparseBareHamiltonian) + (-0.026 * ohms * sparseFactoredHamiltonian)
-    H0 = qt.Qobj(H0)
-
-    # diagonalize the sparse matrix
-    eigenvalues, eigenstates = H0.eigenstates()
-
-    # initial state
-    z2_str = z2_initial(N)
-    z2_index = basisMap[z2_str]
-    psi0 = qt.basis(basisLen, z2_index)
-
-    # -------------------------------
-    #
-    # create the driving hamiltonian
-    #
-    # -------------------------------
-
-    # create H1 operator for QobjEvo!
-    copyBasis = basisList
-    diagH1 = []
-
-    eps = np.random.uniform(-d, d, N)
-    driveWeights = 1.0 + eps
-
-    # switches 0s to -1s and keeps 1s the same for the copyBasis
-    # appends to diagH1 the dot product between each bit string and the 0 -> -1 Z2 state
-    for i in range(basisLen):
-
-        bitString = list(copyBasis[i])
-        bitString = [int(i) for i in bitString]
-
-        z2bitString = list(z2_initial(N))
-        z2bitString = 2 * np.array([int(i) for i in z2bitString]) - 1
-
-        diagH1.append(np.dot(driveWeights * (2 * np.array(bitString) - 1), z2bitString))
-
-    # rows and columns lists for diagonal positions in H1
-    rowH1 = [i for i in range(basisLen)]
-    columnH1 = [i for i in range(basisLen)]
-
-    # creates sparse matrix with diagonals as diagH1 list
-    H1 = csr_matrix((diagH1, (rowH1, columnH1)), shape=[basisLen, basisLen])
+    H1 = csr_matrix((diagH1, (diagLocationH1, diagLocationH1)), shape=[basisLen, basisLen])
     H1 = qt.Qobj(H1)
 
     return H0, H1, eigenvalues, eigenstates, psi0, basisList
@@ -496,10 +360,10 @@ def get_random_freq_scar_ham(N, d=0.0):
 
     return H0, H1_list, eigenvalues, eigenstates, psi0, basisList
 
-def get_qubit_ham(N, wm=1.0, disorder=False, detuning=0.0, random_seed=False):    
+def get_qubit_ham(N, wm=1.0, disorder=False, detuning=0.0, random_seed=False):
+    if not random_seed:
+        np.random.seed(0)
     if disorder:
-        if not random_seed:
-            np.random.seed(0)
         wlist = np.random.uniform(-detuning, detuning, N)
         wlist -= np.mean(wlist)
 
@@ -524,7 +388,7 @@ def get_qubit_ham(N, wm=1.0, disorder=False, detuning=0.0, random_seed=False):
         qH0 += qt.tensor(ops0)
         qH1 += qt.tensor(ops1)
 
-    return qH0, qH1, wm
+    return qH0, qH1
 
 def get_qubit_ham_x(N, detuning=0.0, wm=1.0):
     np.random.seed(0)
