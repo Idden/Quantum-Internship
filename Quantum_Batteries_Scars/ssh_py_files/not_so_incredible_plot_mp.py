9e5c51f7c769308801302182b51c72769c79a043
Idden <idden.tsai@gmail.com>
2026-05-11 10:01:08 -0500

    mp setup complete

--- Quantum_Batteries_Scars/ssh_py_files/not_so_incredible_plot_mp.py
+++ Quantum_Batteries_Scars/ssh_py_files/not_so_incredible_plot_mp.py
@@ -1,4 +1,9 @@
 import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+
 import numpy as np
 import qutip as qt
 from multiprocessing import Pool
