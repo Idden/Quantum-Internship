67f527ae8176a05c0e260ef2da9a0c8a37fa70c1
Idden <idden.tsai@gmail.com>
2026-05-13 17:55:57 -0500

    local main file

--- Quantum_Batteries_Scars/ssh_py_files/test.py
+++ Quantum_Batteries_Scars/ssh_py_files/test.py
@@ -5,21 +5,11 @@ from multiprocessing import Pool
 from quantumScarFunctions import *
 
 
-xlist = np.linspace(0, 0.5, 11)
-ylist = np.linspace(0, 0.5, 11)
-zlist = np.linspace(0, 0.5, 11)
-dslist = np.linspace(0, 2.0, 21)
-ddlist = np.linspace(0, 0.5, 11)
+xlist = np.logspace(-3, 0, 4)
+ylist = np.logspace(-3, 0, 4)
+zlist = np.logspace(-3, 0, 4)
+dslist = np.linspace(0.01, 5.0, 3) # talk about the resonance
+ddlist = np.linspace(0.01, 5.0, 3)
 nlist = np.linspace(4, 18, 8)
 
-parameter_sweep = []
-
-for N in nlist:
-    for x in xlist:
-        for y in ylist:
-            for z in zlist:
-                for ds in dslist:
-                    for dd in ddlist:
-                        parameter_sweep.append((int(N), x, y, z, ds, dd))
-
-print(parameter_sweep)
+print(xlist)
